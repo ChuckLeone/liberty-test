7b69311e6f1e6d591750f6b92c2fca48e16273e0
Chuck Leone <chuckleone@gmail.com>
2022-01-26 16:15:45 -0500

    Update TopBar.jsx
    fixed appbar display issue

--- src/shared-components/TopBar.jsx
+++ src/shared-components/TopBar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 
 const TopBar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex" } }}>
           <Typography variant="h6" component="div">
             Note App
           </Typography>
